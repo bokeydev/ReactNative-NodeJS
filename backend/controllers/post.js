680b3213f0c02e8625994b4383279ffcc34917ae
Bojan Velimirovic <bokey@Bojans-MacBook-Pro.local>
2023-11-14 02:15:17 +0100

    Validating files with multer and incoming data with express-validator, custom express-validator, saving record inside database

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,6 +1,10 @@
 const Post = require("../models/post");
 
-exports.createPost = (req, res) => {
+exports.createPost = async (req, res) => {
   const { title, meta, content, slug, author, tags } = req.body;
-  const post = new Post({ title, meta, content, slug, author, tags });
+  const newPost = new Post({ title, meta, content, slug, author, tags });
+
+  await newPost.save();
+
+  res.json(newPost);
 };
--- backend/routers/post.js
+++ backend/routers/post.js
@@ -1,7 +1,21 @@
 const { createPost } = require("../controllers/post");
+const multer = require("../middleware/multer");
+const { postValidator, validate } = require("../middleware/postValidator");
 
 const router = require("express").Router();
 
-router.post("/create", createPost);
+router.post(
+  "/create",
+  multer.single("thumbnail"),
+  (req, res, next) => {
+    const { tags } = req.body;
+    if (tags) req.body.tags = JSON.parse(tags);
+
+    next();
+  },
+  postValidator,
+  validate,
+  createPost
+);
 
 module.exports = router;
